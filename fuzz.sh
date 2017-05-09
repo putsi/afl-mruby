f080d179ad3141484de1deaf34ded62fc9fe2cbd
Jarmo Puttonen <jarmo.puttonen@linux.com>
2017-05-09 21:04:27 +0300

    Revert afl-fuzz uncapped memory so that server wont run oom

--- fuzz.sh
+++ fuzz.sh
@@ -11,10 +11,10 @@ AFL_OUT_DIR=/results
 
 # Start master and slaves in background and hide any output.
 echo "Starting master-fuzzer"
-afl-fuzz -i $TESTCASE_DIR -o $AFL_OUT_DIR -m none -t 10000 -Mmaster-0 -- /results/mruby @@ >/dev/null &
+afl-fuzz -i $TESTCASE_DIR -o $AFL_OUT_DIR -t 10000 -Mmaster-0 -- /results/mruby @@ >/dev/null &
 for i in $(seq 1 $SLAVE_COUNT); do
     echo "Starting slave $i"
-    afl-fuzz -i $TESTCASE_DIR -o $AFL_OUT_DIR -m none -t 10000 -Sslave-$i -- /results/mruby @@ >/dev/null &
+    afl-fuzz -i $TESTCASE_DIR -o $AFL_OUT_DIR -t 10000 -Sslave-$i -- /results/mruby @@ >/dev/null &
     sleep 1
 done
 
